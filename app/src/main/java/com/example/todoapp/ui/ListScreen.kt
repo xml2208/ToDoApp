9ca8e65fca239fa232baa956cf86695df17546ea
MohinaBonu2208 <mohinabonu2208@gmail.com>
2022-09-08 23:22:53 +0500

    initial

--- app/src/main/java/com/example/todoapp/ui/ListScreen.kt
+++ app/src/main/java/com/example/todoapp/ui/ListScreen.kt
@@ -1,6 +1,5 @@
 package com.example.todoapp.ui
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -14,8 +13,6 @@ import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.colorResource
-import com.example.todoapp.R
 
 @Composable
 fun ListScreen(onPlusClicked: () -> Unit) {
